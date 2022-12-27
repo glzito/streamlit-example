72f16e2cc3177134cef7f55c24047ca69c81446a
glzito <118913799+glzito@users.noreply.github.com>
2022-12-27 23:04:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,44 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
+# DGG Load market data from Binance API
+df = pd.read_json("https://api.binance.com/api/v3/ticker/24hr")
+
+
+crpytoList = {
+    "Price 1": "BTCBUSD",
+    "Price 2": "ETHBUSD",
+    "Price 3": "BNBBUSD",
+    "Price 4": "XRPBUSD",
+    "Price 5": "ADABUSD",
+    "Price 6": "DOGEBUSD",
+    "Price 7": "SHIBBUSD",
+    "Price 8": "DOTBUSD",
+    "Price 9": "MATICBUSD",
+}
+
+col1, col2, col3 = st.columns(3)
+
+for i in range(len(crpytoList.keys())):
+    selected_crypto_label = list(crpytoList.keys())[i]
+    selected_crypto_index = list(df.symbol).index(crpytoList[selected_crypto_label])
+    selected_crypto = st.sidebar.selectbox(
+        selected_crypto_label, df.symbol, selected_crypto_index, key=str(i)
+    )
+    col_df = df[df.symbol == selected_crypto]
+    col_price = round_value(col_df.weightedAvgPrice)
+    col_percent = f"{float(col_df.priceChangePercent)}%"
+    if i < 3:
+        with col1:
+            st.metric(selected_crypto, col_price, col_percent)
+    if 2 < i < 6:
+        with col2:
+            st.metric(selected_crypto, col_price, col_percent)
+    if i > 5:
+        with col3:
+            st.metric(selected_crypto, col_price, col_percent)
+
+# FIN DGG
 
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
